d1b2e144786730ab3c70fa6be5cfa356d9d8143a
VictorFromTheInternet <vma0430@gmail.com>
2025-07-03 13:35:23 -0500

    adding some fonts

--- certificate-generator/src/components/PdfExport.jsx
+++ certificate-generator/src/components/PdfExport.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
+import { Page, Text, Font, View, Image, Document, StyleSheet } from '@react-pdf/renderer';
+
+// fonts
+Font.register({
+	family: "Lato",       
+    src: '/fonts/Lato/Lato-Regular.ttf'
+})
+Font.register({
+	family: "PinyonScript",    	
+    src: '/fonts/Pinyon_Script/PinyonScript-Regular.ttf'
+})
+Font.register({
+	family: "Poppins",    	
+    src: '/fonts/Poppins/Poppins-Regular.ttf'
+})
+
 
 // Create styles
 const styles = StyleSheet.create({
@@ -10,7 +25,29 @@ const styles = StyleSheet.create({
   section: {
     margin: 10,
     padding: 10,
-    flexGrow: 1
+    flexGrow: 1,
+    // backgroundColor: '#FF0000F0'
+  },
+  headingTextSerif:{
+    fontFamily: 'Helvetica',
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#333'
+  },
+  headingTextCursive:{
+    fontFamily: 'PinyonScript',
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#333'
+  },
+  paragraphTextSans:{
+    fontFamily: 'Poppins',
+    fontSize: 24,
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#333'
   }
 });
 
@@ -24,6 +61,8 @@ function MyDocument({students, schoolName, schoolLogo, date}){
                     <Page size="A4" style={styles.page} key={index} orientation="landscape">                       
                             
                         <View style={styles.section}>
+                            <Text style={styles.headingTextSerif}>Certificate</Text>
+                            <Text style={styles.headingTextCursive}>of completion</Text>
                             <Text>Section #1</Text>
                             <Text>Student: {student.name}</Text>
                             <Text>Grade: {student.grade}</Text>
